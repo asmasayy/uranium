c77624316860557ab052aad1ba6896111e79260d
asmasayy <asmasayyed110786@gmail.com>
2022-04-11 21:41:31 +0530

    Assignment

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh87698-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://salman-110:Salman110@cluster0.qfvxy.mongodb.net/salman-110", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
