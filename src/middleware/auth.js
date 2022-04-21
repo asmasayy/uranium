c1c362717f2c39ba00dbe03d460ad0251796e749
asmasayy <asmasayyed110786@gmail.com>
2022-04-22 02:39:17 +0530

    Assignment

---
+++ src/middleware/auth.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken")
+const userModel = require("../models/userModel");
+
+const validateToken = async function(req,res,next){
+    let token = req.headers['x-Auth-Token'] || req.headers['x-auth-token']
+
+    if(!token){
+        res.send({status:false,msg:"token must be present"})
+    }
+    let decodedToken = jwt.verify(token,"functionup-thorium")
+    if(!decodedToken){
+        res.send({status:false,msg:"token is invalid"})
+    }
+
+    next()
+}
+
+const validateUser = async function (req, res,next) {
+    let userId = req.params.userId;
+    let user = await userModel.findById(userId);
+    if (!user) {
+      return res.send("No such user exists");
+    }
+    next()
+}
+module.exports.validateToken= validateToken
+module.exports.validateUser=validateUser
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema( {
+const userSchema = new mongoose.Schema({
     firstName: String,
     lastName: String,
     mobile: {
@@ -14,7 +14,13 @@ const userSchema = new mongoose.Schema( {
         type: String,
         enum: ["male", "female", "other"]
     },
+    isDeleted: {
+        type: Boolean,
+        default: false
+    },
     age: Number,
 }, { timestamps: true });
 
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model('NewUser', userSchema) 
+
+
